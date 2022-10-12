236aef62f8281c9c95bc75fbac68e8d7784beec9
Blanco0420 <sachajamesterry@gmail.com>
2022-10-12 12:31:16 +0100

    Fully Working :DDDD

--- Program.cs
+++ Program.cs
@@ -5,7 +5,12 @@ namespace Calculator
 {
     public class Program
     {
-        static double[] testing(double P,double I,double V,double R){
+        public static string whitespace(int amount, int take){
+            string white = " ";
+            white+= new string(' ', amount-take);
+            return white;
+        }
+        static double[] calculate(double P,double I,double V,double R){
             double[] vals = {P, I, V, R};
             double[] currVals = new double[4];
             for(int i=0; i<vals.Length; i++){
@@ -13,103 +18,40 @@ namespace Calculator
                     currVals[i] = 1;     
                 }
             }
-            Console.WriteLine(currVals[0]);
-            Console.WriteLine(currVals[1]);
-            Console.WriteLine(currVals[2]);
-            Console.WriteLine(currVals[3]);
 
             //List out all different possibilities
-            if(currVals[0] != 0 && currVals[1] != 0){ // P, I
+            if(currVals[0] != 0 && currVals[1] != 0){ // P, I = V
                 vals[2] = P/I;
             }
-            else if(currVals[0] != 0 && currVals[2] != 0){ // P, V
+            else if(currVals[0] != 0 && currVals[2] != 0){ // P, V = I
                 vals[1] = P/V;
             }
-            else if(currVals[0] != 0 && currVals[3] != 0){ // P, R
-                vals[2] = 
+            else if(currVals[0] != 0 && currVals[3] != 0){ // P, R 
+                vals[2] = Math.Sqrt(P/R);
             }
-            else if(currVals[1] != 0 && currVals[2] != 0){ // I, V
-                vals[2] = 
+            else if(currVals[1] != 0 && currVals[2] != 0){ // I, V = P                  NOT SOLVING FOR RRRRR
+                vals[0] = I*V;
+                vals[3] = V/I;
             }
-            else if(currVals[1] != 0 && currVals[3] != 0){ // I, R
-                vals[2] = 
+            else if(currVals[1] != 0 && currVals[3] != 0){ // I, R = V
+                vals[2] = I*R;
             }
-            else if(currVals[2] != 0 && currVals[3] != 0) // V, R
-            {
-                vals[2] = 
+            else if(currVals[2] != 0 && currVals[3] != 0){ // V, R = I
+                vals[1] = V/R;
             }
-            
-            // Dictionary<char, double> results = new Dictionary<char, double>(){
-            //     {'P', default(int)},
-            //     {'I', default(int)},
-            //     {'V', default(int)},
-            //     {'R', default(int)},
-            // };
-            // char[] chars = {'P', 'I', 'V', 'R'};
-            // for(int i=0; i<vals.Length; i++){
-            //     if(vals[i] != 0){
-            //         results[chars[i]] = vals[i];
-            //     }
-            // }
-            // if(results[1] == 0){
-            //     results[1] = V/R;  
-            // }
-            // if(results[2] == 0){
-            //     results[2] = I*R;
-            // }
-            // if(results[3] == 0){
-            //     results[3] = V/I;
-            // }
-            // if(results[0] == 0){
-            //     results[0] = I*V;
-            // }
-            // for(int i=0; i<results.Length; i++){
-            //     if(results[i] == 0){
-            //         Console.WriteLine($"{i} is 0");
-            //     }
-            // }
-            return currVals;
-        }
-        static double[] calculate(double P = default(int), double I = default(int), double V = default(int), double R = default(int)){
-            double[] results = new double[4];
-            if(V == 0){
-                results[2] = cvoltage(I, R);
+            if(vals[0] == 0){
+                vals[0] = vals[1] * vals[2];
             }
-            if(I == 0){
-                results[1] = ccurrent(V, R);
+            else if(vals[1] == 0){
+                vals[1] = vals[0] / vals[2];
             }
-            if(R == 0){
-                results[3] = cresistance(V, I);
+            else if(vals[2] == 0){
+                vals[2] = vals[1] * vals[3];
             }
-            if(P == 0){
-                if(I == 0){
-                    results[1] = ccurrent(V, R);
-                }
-                else{
-                    results[2] = cvoltage(I, R);
-                }
-                results[0] = cpower(I, V);
-                if(R == 0){
-                    results[3] = cresistance(V, I);
-                }
-                for(int i = 0; i<5; i++){
-                Console.WriteLine(results[i]);
-                }
+            else if(vals[3] == 0){
+                vals[3] = vals[2] / vals[1];
             }
-            return results;
-        //     I = V/R;
-        //     R = V/I;
-        //     V = I*R;
-        //     P = I*V;
-        //     results[0] = P;
-        //     results[1] = I;
-        //     results[2] = V;
-        //     results[3] = R;
-        //     if(P == 0){
-        //         Console.WriteLine("P == 0");
-        //         return results;
-        //     }
-        //     return results;
+            return vals;
         }
         static double cpower(double x, double y, int which = 0) {
             switch(which) {
@@ -189,7 +131,7 @@ namespace Calculator
             };
             string[] vals = {"P", "I", "V", "R"};
             int counter= 0;
-            while(counter !=4){
+            while(counter !=2){
                 Console.WriteLine("Please enter some data using these units: P,I,V,R (e.g. V=54.753) (to go back, type back)");
                 Console.WriteLine(String.Format("P = {0}, I = {1}, V = {2}, R = {3}", data['P'], data['I'], data['V'], data['R']));
                 string curr = Console.ReadLine();
@@ -209,11 +151,11 @@ namespace Calculator
                 }
                 catch(Exception) {
                     if(curr == ""){
-                        if(counter >=2){
+                        if(counter >2){
                             break;
                         }
                         else{
-                            Console.WriteLine("Error, at least 2 values are required.");
+                            Console.WriteLine("Error, 2 values are required.");
                         }
                     }
                     else if(curr == "back"){
@@ -225,9 +167,24 @@ namespace Calculator
                 }
             }
             double[] results = calculate(data['P'],data['I'],data['V'],data['R']);
+            int length = default(int);
             for(int i=0; i<results.Length; i++){
-                Console.WriteLine($"{vals[i]}, {results[i]}");
+                if(length < (results[i].ToString()).Length){
+                    length = (results[i].ToString()).Length;
+                }          
             }
+            string barrier = new string('=', length+8);
+            string[] lengths = new string[4];
+            for(int i=0; i<4; i++){
+                int vars = Convert.ToString(results[i]).Length;
+                lengths[i] = (whitespace(length, vars) + "||");
+            }
+            Console.WriteLine("//" + barrier + "\\");
+            Console.WriteLine($"|| P -- {results[0]}" + lengths[0]);
+            Console.WriteLine($"|| I -- {results[1]}" + lengths[1]);
+            Console.WriteLine($"|| V -- {results[2]}" + lengths[2]);
+            Console.WriteLine($"|| R -- {results[3]}" + lengths[3]);
+            Console.WriteLine("\\" + barrier + "//");
         }
         private static void menu() {
             Console.WriteLine("Welcome! Please choose from the following:\n 1) Basic\n 2) Advanced");
@@ -247,12 +204,7 @@ namespace Calculator
         }
         public static void Main()
         {
-            double[] results = testing(0, 3, 0, 5);
-            // for(int i=0; i<results.Length; i++){
-            //     Console.WriteLine(results[i]);
-            // }
-            
-            // menu();
+           menu();
         }
     }
 }
